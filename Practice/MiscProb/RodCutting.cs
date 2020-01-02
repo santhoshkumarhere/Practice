72a1d6f2589af8bae1a8495d924a609915696fbf
santhoshkumarhere <santhoshkumarhere@gmail.com>
2020-01-01 18:24:29 -0600

    rod cutting

--- Practice/MiscProb/RodCutting.cs
+++ Practice/MiscProb/RodCutting.cs
@@ -9,8 +9,10 @@ namespace Practice.MiscProb
 
         public static void Test()
         {
-            int[] price = { 1, 5, 8, 9, 10, 17, 17, 20 };
-            var x = RodCut(price, 4);
+            //int[] price = { 1, 5, 8, 9, 10, 17, 17, 20 };
+            var price = new int[] { 3, 5, 8, 9, 10, 17, 17, 20 };
+            var x = RodCut(price, 8);
+            x = RodCut2(price, 8);
         }
 
         private static int RodCut(int[] price, int n)
@@ -23,7 +25,7 @@ namespace Practice.MiscProb
             var max_val = int.MinValue;
             for(var i=1; i <= n; i++)
             {
-                max_val = Math.Max(max_val, price[i - 1] + RodCut(price, n - i));
+                max_val = Math.Max(max_val, price[i - 1] + RodCut(price, n - i)); //the recursive in this line will do combination check like {1,1,1,1}, if you remove recursion then combination check will not happen
             }
             return max_val;
         }
