59a2678fd7817b868bb9beb936f437ed11edf706
santhoshkumarhere <santhoshkumarhere@gmail.com>
2020-02-22 08:14:53 -0600

    fixxbuxx

---
+++ Practice/SixtyDaysChallenge/FizzzBuzz(E).cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.SixtyDaysChallenge
+{
+    public class FizzzBuzz_E_
+    {
+        public static void Test()
+        {
+
+        }
+
+        public static IList<string> FizzBuzz(int n)
+        {
+            var list = new List<string>();
+            for(var i=1; i <= n; i++)
+            {
+                var threeMod = i % 3;
+                var fiveMod = i % 5;
+                if(threeMod == 0 && fiveMod == 0)
+                {
+                    list.Add("FizzBuzz");
+                }
+                else if(fiveMod == 0)
+                {
+                    list.Add("Buzz");
+                }
+                else if( threeMod == 0)
+                {
+                    list.Add("Fizz");
+                }
+                else
+                {
+                    list.Add(i.ToString());
+                }
+            }
+            return list;
+        }
+    }
+}
