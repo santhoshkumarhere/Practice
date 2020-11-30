fa556f49a84b964386def674ff5c6620f098f500
Kumar, Santhosh <santhoshk.kumar@pioneer.com>
2020-11-30 16:27:36 -0600

    added reverse integer

--- Practice/NickWhiteLeetCode/IntegerPalindrome.cs
+++ Practice/NickWhiteLeetCode/IntegerPalindrome.cs
@@ -11,6 +11,36 @@ namespace Practice.NickWhiteLeetCode
             var result = IsPalindrome(1001);
         }
 
+        private static bool IsPalindromeLatest(int input)
+        {
+            if (input == 0)
+            {
+                return true;
+            }
+
+            if (input < 0 || input % 10 == 0)
+            {
+                return false;
+            }
+
+            int reversed = 0;
+            while (input > reversed)
+            {
+                var rem =  input % 10;
+                input /= 10;
+                reversed = (reversed * 10) + rem;
+            }
+            //12321
+            if (input == reversed || input == reversed/10) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         private static bool IsPalindrome(int input)
         {
             if(input == 0)
