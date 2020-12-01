366d7cdcf29aa8dddd1cb74203c4219c4b76e331
Kumar, Santhosh <santhoshk.kumar@pioneer.com>
2020-12-01 17:18:07 -0600

    more binary tree problems

---
+++ Practice/NickWhiteLeetCode/BinaryTreePathSum.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Collections.Generic;
+
+namespace Practice.Tree.TreeProblems
+{
+    public class BinaryTreePathSum
+    {
+        // this problem is similar to depth of a tree
+
+        private bool CheckHasPathSum(TreeNode root, int sum)
+        {
+            return HasPathSum(root, sum);
+        }
+
+        private bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null || (root.left == null && root.right == null && root.val != sum))
+            {
+                return false;
+            }
+            if (root.left == null && root.right == null && root.val == sum)
+            {
+                return true;
+            }
+
+            return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+        }
+
+        private bool HasPathSumIterative(TreeNode root, int sum)
+        {
+            if (root == null) return false;
+
+            var sumStack = new Stack<int>();
+            var nodeStack = new Stack<TreeNode>();
+
+            nodeStack.Push(root);
+            sumStack.Push(sum - root.val);
+
+            while(nodeStack.Count > 0)
+            {
+                var curr = nodeStack.Pop();
+                var currSum = sumStack.Pop();
+
+                if (curr.left == null && curr.right == null && currSum == 0) return true;
+
+                if(curr.left != null)
+                {
+                    nodeStack.Push(curr.left);
+                    sumStack.Push(currSum - curr.left.val);
+                }
+
+                if (curr.right != null)
+                {
+                    nodeStack.Push(curr.right);
+                    sumStack.Push(currSum - curr.right.val);
+                }
+            }
+            return false;
+        }
+    }
+}
