9104220d9cc4abf1c1d50aaf739cb30139a661e4
santhoshkumarhere <santhoshkumarhere@gmail.com>
2020-12-15 07:34:34 -0600

    added 3 sum solution

--- Practice/NickWhiteLeetCode/3Sum.cs
+++ Practice/NickWhiteLeetCode/3Sum.cs
@@ -8,10 +8,10 @@ namespace Practice.NickWhiteLeetCode
     {
         public static void Test()
         {
-            //var nums = new int[] { -1, 0, 1, 2, -1, -4 };
-           // var result = ThreeSum(nums);
-            var nums = new int[] { -5, -3, -2, 0, 1, 3, 5 };
+            var nums = new int[] { -1, 0, 1, 2, -1, -4 };
             var result = ThreeSum(nums);
+             nums = new int[] { -5, -3, -2, 0, 1, 2, 4, 5 };
+             result = ThreeSum(nums);
         }
         private static IList<IList<int>> ThreeSum(int[] nums)
         {
@@ -20,9 +20,31 @@ namespace Practice.NickWhiteLeetCode
             if (nums == null || nums.Length <= 2) return res;
             
             Array.Sort<int>(nums);
+
             
-            for(var i = 0; i<= nums.Length - 3; i++)
+            for(var i = 0; i < nums.Length - 2; i++)
             {
+                if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) // (1) process first record (2) skip duplicates, since we processed it already
+                {
+                    int left = i + 1, right = nums.Length - 1;
+                    while (left < right)
+                    {
+                        var remainingNeeded = 0 - nums[i];
+                        var sum = nums[left] + nums[right];
+
+                        if (remainingNeeded == sum)
+                        {
+                            var list = new List<int>() { nums[i], nums[left], nums[right] };
+                            res.Add(list);
+                            while (left < right && nums[left] == nums[left + 1]) left++;
+                            while (left < right && nums[right] == nums[right - 1]) right--;
+                            left++;
+                            right--;
+                        }
+                        else if (sum < remainingNeeded) left++;
+                        else if (sum > remainingNeeded) right--;
+                    }
+                }
             }
 
             return res;
