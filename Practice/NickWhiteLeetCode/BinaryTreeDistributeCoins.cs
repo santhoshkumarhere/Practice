c2de3890ab987f1cf0cfacc0d823cceba9f61888
santhoshkumarhere <santhoshkumarhere@gmail.com>
2020-12-29 10:09:45 -0600

    distribute coins in BT

---
+++ Practice/NickWhiteLeetCode/BinaryTreeDistributeCoins.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.NickWhiteLeetCode.TreeProblems
+{
+    public class BinaryTreeDistributeCoins
+    {
+        int moves = 0;
+        private int DistributeCoins(TreeNode root)
+        {
+            if(root == null)
+            {
+                return 0;
+            }
+
+            int left = DistributeCoins(root.left);
+            int right = DistributeCoins(root.right);
+            moves += Math.Abs(left) + Math.Abs(right);
+            return root.val + left + right - 1;
+        }
+    }
+}
