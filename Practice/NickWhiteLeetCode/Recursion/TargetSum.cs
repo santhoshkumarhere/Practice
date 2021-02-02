83b5f0c71dd83e4d97c069c14f1cc5dde13c51d3
santhoshkumarhere <santhoshkumarhere@gmail.com>
2021-02-02 06:19:44 -0600

    memoization of target sum

--- Practice/NickWhiteLeetCode/Recursion/TargetSum.cs
+++ Practice/NickWhiteLeetCode/Recursion/TargetSum.cs
@@ -17,11 +17,13 @@ namespace Practice.NickWhiteLeetCode.Recursion
         private static void FindTargetSumWays(int[] nums, int S)
         {
 
-            FindWays(nums, S, 0, 0);
+            // FindWays(nums, S, 0, 0);
+            var memo = new Dictionary<(int, int), int>();
+            var res = FindWaysMemoization(nums, S, 0, 0, memo);
         }
 
         private static void FindWays(int[] nums, int S, int i, int currSum)
-        {
+        { //this is also passed in leetcode
             if (i == nums.Length)
             {
                 if (currSum == S)
@@ -33,6 +35,27 @@ namespace Practice.NickWhiteLeetCode.Recursion
             
             FindWays(nums, S, i + 1, currSum + nums[i]);
             FindWays(nums, S, i + 1, currSum - nums[i]);            
+        }
+
+        private static int FindWaysMemoization(int[] nums, int S, int i, int currSum, Dictionary<(int, int), int> memo)
+        {
+            if (i == nums.Length)
+            {
+                if (currSum == S)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+            if(memo.ContainsKey((i, currSum)))
+            {
+                return memo[(i, currSum)];
+            }
+            int add = FindWaysMemoization(nums, S, i + 1, currSum + nums[i], memo);
+            int sub = FindWaysMemoization(nums, S, i + 1, currSum - nums[i], memo);
+            memo[(i, currSum)] = add + sub;
+
+            return memo[(i, currSum)];
         }
     }
 }
