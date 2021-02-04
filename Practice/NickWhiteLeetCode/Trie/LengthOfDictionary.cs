9047510a71f1b7b446fc752a219efb3d28c3d340
kumars9 <santhoshk.kumar@corteva.com>
2021-02-03 18:35:17 -0600

    checked in right solution for prefix longestword in dictionary

--- Practice/NickWhiteLeetCode/Trie/LengthOfDictionary.cs
+++ Practice/NickWhiteLeetCode/Trie/LengthOfDictionary.cs
@@ -10,16 +10,42 @@ namespace Practice.NickWhiteLeetCode.Trie
 
         public static void Test()
         {
-           // LongestWord(new string[] { "w", "wo", "wor", "worl", "world" });
-          //  LongestWord(new string[] { "a", "banana", "app", "appl", "ap", "apply", "apple" });
-            LongestWord(new string[] { "ts", "e", "x", "pbhj", "opto", "xhigy", "erikz", "pbh", "opt", "erikzb", "eri", "erik", "xlye", "xhig", "optoj", "optoje", "xly", "pb", "xhi", "x", "o" });
-            LongestWord(new string[] { "a" });
+           // var res1= LongestWord(new string[] { "w", "wo", "wor", "worl", "world" });
+            var res2 = LongestWord(new string[] { "a", "banana", "app", "appl", "ap", "apply", "apple" });
+            var res3 = LongestWord(new string[] { "ts", "e", "x", "pbhj", "opto", "xhigy", "erikz", "pbh", "opt", "erikzb", "eri", "erik", "xlye", "xhig", "optoj", "optoje", "xly", "pb", "xhi", "x", "o" });
+            var res4 = LongestWord(new string[] { "a" });
         }
 
         private static string LongestWord(string[] words)
         {
+            var wordset = new HashSet<string>(words);
+
+            Array.Sort(words, (a, b) => a.Length == b.Length
+                        ? a.CompareTo(b) 
+                        : b.Length - a.Length);
+            
+            foreach (string word in words)
+            {
+                var good = true;
+                for (int k = 1; k < word.Length; ++k)
+                {
+                    if (!wordset.Contains(word.Substring(0, k)))
+                    {
+                        good = false;
+                        break;
+                    }
+                }
+                if (good) return word;
+            }
+
+            return "";
+        }
+
+        private static string LongestWordMy(string[] words)
+        {
             if (words.Length == 1) return words[0];
 
+            var wordSet = new HashSet<string>(words); //run time goes down from 2000ms to 650ms by changing it to set from List
             var sortedWords = words.OrderByDescending(x => x.Length);
             var result = new SortedDictionary<string, string>();
             int currentMaxWord = 0;
@@ -28,12 +54,40 @@ namespace Practice.NickWhiteLeetCode.Trie
             {
                 if (word.Length >= currentMaxWord)
                 {
-                  currentMaxWord = Math.Max(currentMaxWord, DFS(0, word, string.Empty, result, words));
+                  currentMaxWord = Math.Max(currentMaxWord, DFS2(0, word, string.Empty, result, wordSet));
                 }
                 else
                     break;
             }
             return result.Keys.Count > 0 ? result.Keys.First() : "";
+        }
+
+        private static int DFS2(int start, string word, string track, SortedDictionary<string, string> result, HashSet<string> wordSet)
+        {
+            if (word.Length == 1 && !result.ContainsKey(word) && wordSet.Contains(word)) //words with 1 character
+            {
+                result.Add(word, track);
+                return word.Length;
+            }
+
+            for (int i = start; i < word.Length - 1; i++)
+            {
+                track = track + word[i];
+                if (wordSet.Contains(track))
+                {
+                    if (track.Length == word.Length - 1 && !result.ContainsKey(word) && wordSet.Contains(track))
+                    {
+                        result.Add(word, track);
+                        return word.Length;
+                    }
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return 0;
         }
 
         private static int DFS(int start, string word, string track, SortedDictionary<string, string> result, string[] wordList)
