5db070a659be9bf5f2f671a0f81ef87df3664524
kumars9 <santhoshk.kumar@corteva.com>
2021-03-01 10:26:00 -0600

    double recursion

--- Practice/NickWhiteLeetCode/DP/PartitionEqualSubSetSum.cs
+++ Practice/NickWhiteLeetCode/DP/PartitionEqualSubSetSum.cs
@@ -18,7 +18,9 @@ namespace Practice.NickWhiteLeetCode.DP
 
             var subset = totalSum / 2;
             var memo = new Dictionary<(int, int), bool>();
-            var res = DFS(subset, 0, nums, memo);
+            //var res = DFS(subset, 0, nums, memo);
+
+            var res = DFSLeet(nums, 0, subset, memo);
         }
 
         private static bool DFS(int remaining, int start, int[] nums, Dictionary<(int, int), bool> memo)
@@ -41,6 +43,23 @@ namespace Practice.NickWhiteLeetCode.DP
             }
             memo[(start, remaining)] = false;
             return false;
+        }
+
+        public static bool DFSLeet(int[] nums, int n, int subSetSum, Dictionary<(int, int), bool> memo)
+        {
+            if (subSetSum == 0)
+                return true;
+
+            if (n == nums.Length - 1 || subSetSum < 0)
+                return false;
+
+            if (memo.ContainsKey((n, subSetSum)))
+                return memo[(n, subSetSum)];
+
+            var result = DFSLeet(nums, n + 1 , subSetSum - nums[n + 1], memo) ||  DFSLeet(nums, n + 1, subSetSum, memo);
+
+            memo[(n, subSetSum)] = result;
+            return result;
         }
     }
 }
