efc9793e8e69e68c31cf905aa4d46ddd87200f4d
santhoshkumarhere <santhoshkumarhere@gmail.com>
2021-03-03 06:46:00 -0600

    some files

--- Practice/NickWhiteLeetCode/DP/DecodeWays.cs
+++ Practice/NickWhiteLeetCode/DP/DecodeWays.cs
@@ -27,7 +27,7 @@ namespace Practice.NickWhiteLeetCode.DP
             if (s[0] != '0')
                 dp[1] = 1;
 
-            for(int i = 2; i < dp.Length; i++)
+            for(int i = 2; i < dp.Length; i++) //looping wiht dp length so we are validating current string and previous string
             {
                 dp[i] = s[i-1] != '0' ? dp[i - 1] : 0;
                 
