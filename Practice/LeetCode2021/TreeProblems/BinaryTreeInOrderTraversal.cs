09657bae777e05230b110f3eccc03fb4c59ecf36
santhosh <santhoshk.kumar@corteva.com>
2021-04-13 08:31:17 -0500

    Binary tree Inorder traversal

---
+++ Practice/LeetCode2021/TreeProblems/BinaryTreeInOrderTraversal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LeetCode2021.TreeProblems
+{
+    public class BinaryTreeInOrderTraversal
+    {
+        public static void Test()
+        {
+            var tree = new TreeNode(1);
+            tree.right = new TreeNode(2);
+            tree.right.left = new TreeNode(3);
+
+            var res = InorderTraversal(tree);
+        }
+
+        private static IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+            var curr = root;
+
+            var stack = new Stack<TreeNode>();
+            var result = new List<int>();
+            while(curr != null || stack.Count > 0)
+            {
+                while(curr != null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+
+                var top = stack.Pop();
+
+                result.Add(top.val);
+
+                if (top.right != null)
+                    curr = top.right;
+            }
+
+            return result;
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -38,7 +38,8 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            JumpGameII_Greedy_.Test();
+            BinaryTreeInOrderTraversal.Test();
+            //JumpGameII_Greedy_.Test();
             //Search2DMatrixII.Test();
             //FindDuplicateTortoiseHareFloyd.Test();
             //ConstructBinaryTreeFromIn_PreOrder.Test();
