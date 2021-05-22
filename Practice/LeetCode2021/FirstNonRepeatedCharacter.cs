1788f80315d6478025708d5b3e8196e9e48318e4
santhosh <santhoshk.kumar@corteva.com>
2021-05-22 18:17:28 -0500

    First non repeated character in a string

---
+++ Practice/LeetCode2021/FirstNonRepeatedCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LeetCode2021
+{
+    public class FirstNonRepeatedCharacter
+    {
+        public static void Test()
+        {
+            var s = FirstUniqChar("aaabcddde");
+        }
+
+        public static int FirstUniqChar(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return -1;
+
+            var map = new Dictionary<char, int>();
+
+            foreach(var c in s)
+            {
+                if(map.ContainsKey(c))
+                {
+                    map[c]++;
+                }
+                else
+                {
+                    map.Add(c, 1);
+                }
+            }
+
+            for(var i = 0; i < s.Length; i++)
+            {
+                if (map[s[i]] == 1)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
