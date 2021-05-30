78e1c0ff907a15b6991c735e04e4e801bdd729d2
santhosh <santhoshk.kumar@corteva.com>
2021-05-30 15:03:51 -0500

    domino pair

---
+++ Practice/LeetCode2021/Arrays/NoOfEquivalentDominoes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LeetCode2021.Arrays
+{
+    public class NoOfEquivalentDominoes
+    {
+
+        public static void Test()
+        {
+            var dominoes = new int[][]
+            {
+                new int[]{1, 2},
+                new int[]{2, 1},
+                new int[]{3, 4},
+                new int[]{5, 6},
+            };
+            var res = NumEquivDominoPairs(dominoes);
+        }
+
+        private static int NumEquivDominoPairs(int[][] dominoes)
+        {
+            var result = 0;
+            var dict = new Dictionary<int, int>();
+            foreach (var d in dominoes)
+            {
+                //good hashing
+                var key = Math.Min(d[0], d[1]) * 10 + Math.Max(d[0], d[1]);
+
+                if (dict.ContainsKey(key))
+                {
+                    result += dict[key];
+                    dict[key]++;
+                }
+                else
+                {
+                    dict[key] = 1;
+                }
+
+            }
+            return result;
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -39,7 +39,8 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            TwoSumBST.Test();
+            NoOfEquivalentDominoes.Test();
+            //TwoSumBST.Test();
             //LongestSubstringAtMostKDistinctChars.Test();
             //LongestSubsWithAlmostTwoDisctinctCharacters.Test();
             //LongestSubstringWithOutRepeatingChar.Test();
