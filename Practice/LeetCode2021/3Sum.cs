d24258afe31db7e42c4688920ef2a4a76008379c
santhosh <santhoshk.kumar@corteva.com>
2021-10-22 17:06:43 -0500

    3 Sum

--- Practice/LeetCode2021/3Sum.cs
+++ Practice/LeetCode2021/3Sum.cs
@@ -13,7 +13,53 @@ namespace Practice.LeetCode2021
              nums = new int[] { -5, -3, -2, 0, 1, 2, 4, 5 };
              result = ThreeSum(nums);
         }
+
         private static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var res = new List<IList<int>>();
+            if (nums == null || nums.Length <= 2) return res;
+
+            Array.Sort<int>(nums);
+
+            for (var i = 0; i < nums.Length && nums[i] <= 0; i++)
+            {
+                if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) // (1) process first record (2) skip duplicates, since we processed it already
+                {
+                    TwoSum(nums, i, res);
+                }
+            }
+
+            return res;
+        }
+
+        private static void TwoSum(int[] nums, int i, List<IList<int>> res)
+        {
+            int left = i + 1, right = nums.Length - 1;
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum < 0)
+                {
+                    left++;
+                }
+                else if (sum > 0)
+                {
+                    right--;
+                }
+                else
+                {
+                    res.Add(new List<int> { nums[i], nums[left], nums[right] });
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        private static IList<IList<int>> ThreeSumII(int[] nums)
         {
             var res = new List<IList<int>>();
 
@@ -38,8 +84,8 @@ namespace Practice.LeetCode2021
                             res.Add(list);
                             while (left < right && nums[left] == nums[left + 1]) left++;
                             while (left < right && nums[right] == nums[right - 1]) right--;
-                            left++;
-                            right--;
+                            left++; // there are no chance that any other with current left make it to zero
+                            right--; // there are no chance that any other with current left make it to zero
                         }
                         else if (sum < remainingNeeded) left++;
                         else if (sum > remainingNeeded) right--;
