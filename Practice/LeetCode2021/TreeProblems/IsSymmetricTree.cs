6e0a11d8d200e1fc9a9e638ecd2e5f73a3774d12
santhosh <santhoshk.kumar@corteva.com>
2021-11-01 15:15:17 -0500

    Symmetric queue iterative

--- Practice/LeetCode2021/TreeProblems/IsSymmetricTree.cs
+++ Practice/LeetCode2021/TreeProblems/IsSymmetricTree.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Generic;
+
 namespace Practice.Tree.TreeProblems
 {
     public class IsSymmetricTree
@@ -19,6 +21,30 @@ namespace Practice.Tree.TreeProblems
             if (t1 == null || t2 == null) return false;
 
             return t1.val == t2.val && IsMirror(t1.left, t2.right) && IsMirror(t1.right, t2.left);
+        }
+
+        private static bool IsMirrorRecursive(TreeNode root)
+        {
+            var q = new Queue<TreeNode>();
+
+            q.Enqueue(root);
+            q.Enqueue(root);
+
+            while(q.Count > 0)
+            {
+                TreeNode t1 = q.Dequeue();
+                TreeNode t2 = q.Dequeue();
+
+                if (t1 == null && t2 == null) continue;
+                if (t1 == null || t2 == null) return false;
+                if (t1.val != t2.val) return false;
+
+                q.Enqueue(t1.left);
+                q.Enqueue(t2.right);
+                q.Enqueue(t1.right);
+                q.Enqueue(t2.left);
+            }
+            return true;
         }
     }
 }
