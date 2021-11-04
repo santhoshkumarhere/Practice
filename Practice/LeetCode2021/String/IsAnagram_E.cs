26b4851ec417891f8ce946bb9c1979c360050ccf
santhosh <santhoshk.kumar@corteva.com>
2021-11-04 13:43:02 -0500

    anagram

---
+++ Practice/LeetCode2021/String/IsAnagram_E.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LeetCode2021.String
+{
+    public class IsAnagram_E
+    {
+        public static void Test()
+        {
+            var res = IsAnagram("anagram", "nagarm");
+        }
+
+        private static bool IsAnagram(string s, string t)
+        {
+
+            if (s.Length != t.Length)
+                return false;
+            var map = new int[26];
+            foreach (var c in s)
+            {
+                map[c - 'a']++;
+            }
+
+            foreach (var c in t)
+            {
+                if (map[c - 'a'] == 0)
+                    return false;
+                map[c - 'a']--;
+            }
+            return true;
+        }
+    }
+}
