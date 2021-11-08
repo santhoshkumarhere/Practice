648bb49f1e2c63b82c27e3d003f1dc1fe7bb3ed6
santhoshkumarhere <santhoshkumarhere@gmail.com>
2021-11-08 07:33:44 -0600

    tree problems solved

---
+++ Practice/LeetCode2021/TreeProblems/BalancedBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LeetCode2021.TreeProblems
+{
+    public class BalancedBinaryTree
+    {
+
+        public bool IsBalanced(TreeNode root)
+        {
+            return FindDepth(root) != -1;
+        }
+
+        private int FindDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int leftDepth = FindDepth(root.left);
+            int rightDepth = FindDepth(root.right);
+
+            if (leftDepth == -1 || rightDepth == -1 || Math.Abs(leftDepth - rightDepth) > 1)
+                return -1;
+            return 1 + Math.Max(leftDepth, rightDepth);
+        }
+
+    }
+}
---
+++ Practice/LeetCode2021/TreeProblems/DiameterBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LeetCode2021.TreeProblems
+{
+    class DiameterBinaryTree
+    {
+        private int diameter = 0;
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            LengthOfTree(root);
+            return diameter;
+        }
+
+        private int LengthOfTree(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int leftTreeLength = LengthOfTree(root.left);
+            int rightTreeLength = LengthOfTree(root.right);
+            diameter = Math.Max(diameter, leftTreeLength + rightTreeLength); // do not add 1 + here for root. because it was already added in return statement during recursion
+
+            return 1 + Math.Max(leftTreeLength, rightTreeLength);
+            
+        }
+    }
+}
