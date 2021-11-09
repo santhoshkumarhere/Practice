8ac3174781969bc137de139c4b30614494d26c3d
santhosh <santhoshk.kumar@corteva.com>
2021-11-09 17:54:23 -0600

    added tilt

---
+++ Practice/LeetCode2021/TreeProblems/BinaryTreeTilt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LeetCode2021.TreeProblems
+{
+   public class BinaryTreeTilt
+    {
+        private int tilt = 0;
+        public int FindTilt(TreeNode root)
+        {
+            this.GetSum(root);
+            return tilt;
+        }
+
+        private int GetSum(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            int leftSum = GetSum(node.left);
+            int rightSum = GetSum(node.right);
+            tilt += Math.Abs(leftSum - rightSum);
+
+            return node.val + leftSum + rightSum;
+        }
+    }
+}
--- Practice/LeetCode2021/TreeProblems/IsSameTree(E).cs
+++ Practice/LeetCode2021/TreeProblems/IsSameTree(E).cs
@@ -20,12 +20,9 @@ namespace Practice.LeetCode2021.TreeProblems
 
         private static bool IsSameTree(TreeNode p, TreeNode q)
         {
-            if (p == null && q == null)
-                return true;
-            if (p == null || q == null)
-                return false;
-            if (p.val != q.val)
-                return false;
+            if (p == null && q == null) return true;
+            if (p == null || q == null) return false;
+            if (p.val != q.val) return false;
 
             return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
         }
