b01d2081aa8563315eec65919faaba7adf5c2dae
santhosh <santhoshk.kumar@corteva.com>
2022-01-19 15:19:40 -0600

    rotate matrix

--- Practice/LeetCode2021/Matrix/RotateMatrix.cs
+++ Practice/LeetCode2021/Matrix/RotateMatrix.cs
@@ -17,7 +17,35 @@ namespace Practice.LeetCode2021.Matrix
                     new int[]{7, 8, 9}
                 };
 
+            int[][] input2 =
+               {
+                    new int[]{1,  2,  3,  4,  5},
+                    new int[]{6,  7,  8,  9,  10},
+                    new int[]{11, 12, 13, 14, 15},
+                    new int[]{16, 17, 18, 19, 20},
+                    new int[]{21, 22, 23, 24, 25}
+                };
+
             RotateClockwise(input);
+        }
+
+        private static void RotateClockwise2022(int[][] mat)
+        {
+            var length = mat.Length;
+
+            for(int row = 0; row < length / 2; row++) // image in 5x5 matrix we have three layers 0 < 3 -> we have to process three outer layers
+            {
+                for(int col = row; col < length - row - 1; col++) // tip - (0, 4) no to be processed.  tip 2 - after each rotation column right and let side decrement by one
+                {
+                    //see row/col swap
+                    var temp = mat[row][col];
+
+                    mat[row][col] = mat[length - col - 1][row]; //[(0, 0) (4, 0)] [(0, 1) (3, 0)] [(0, 2) (2, 0)] => 0th row 0th colomn
+                    mat[length - col - 1][row] = mat[length - row - 1][length - col - 1]; // (4, 0) (4, 4)
+                    mat[length - row - 1][length - col - 1] = mat[col][length - row - 1];  //(4, 4) (0, 4)) 
+                    mat[col][length - row - 1] = temp;
+                }
+            }
         }
 
         private static void RotateClockwise(int[][] mat)
