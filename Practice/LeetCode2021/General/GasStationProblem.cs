49d9368c632125d7acc131339cf757a92da51d2b
santhosh <santhoshk.kumar@corteva.com>
2022-01-24 16:34:38 -0600

    gas station

--- Practice/LeetCode2021/General/GasStationProblem.cs
+++ Practice/LeetCode2021/General/GasStationProblem.cs
@@ -25,11 +25,11 @@ namespace Practice.LeetCode2021.General
             {
                 if(currTank <  0)
                 {
-                    startingPos = i;
+                    startingPos = i; //when you come to my gas station if a running tank is negation then my gas station is a starting point
                     currTank = 0;
                 }
                 var travelCost = gas[i] - cost[i];
-                totalTank += travelCost;
+                totalTank += travelCost; // total tank and cost
                 currTank += travelCost;
             }
 
