a713f1985f3d322d84dd855a9c9b9a5bfd024098
santhosh <santhoshk.kumar@corteva.com>
2022-01-31 17:26:45 -0600

    ma depth

---
+++ Practice/LeetCode2021/TreeProblems/BinaryTreeMaximumDepth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.LeetCode2021.TreeProblems
+{
+    internal class BinaryTreeMaximumDepth
+    {
+        public static int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            int left = MaxDepth(root.left);
+            int right = MaxDepth(root.right);
+
+            return 1 + Math.Max(left, right);
+        }
+    }
+}
